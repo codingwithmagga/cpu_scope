5fc537653a0cd812b734e582e9c60423cfbfebe9
codingwithmagga <codingwithmagga@gmail.com>
2026-05-05 21:10:43 +0200

    Add pid/cpu check

--- lib/src/perf_event.cpp
+++ lib/src/perf_event.cpp
@@ -1,6 +1,11 @@
 #include "perf_event.hpp"
 
-bool PerfEvent::open(const Config& /*config*/) noexcept
+bool PerfEvent::open(const Config& config) noexcept
 {
+    if (config.pid < 0 || config.cpu < -1)
+    {
+        return false;
+    }
+
     return true;
 }
--- tests/test_perf_event.cpp
+++ tests/test_perf_event.cpp
@@ -5,6 +5,19 @@
 TEST(PerfEvent, OpenPerfEvent)
 {
     PerfEvent::Config config;
+
     auto res = PerfEvent::open(config);
+
     EXPECT_TRUE(res);
+}
+
+TEST(PerfEvent, OpenPerfEventWithInvalidConfig)
+{
+    PerfEvent::Config config;
+    config.pid = -1;
+    config.cpu = -1;
+
+    auto res = PerfEvent::open(config);
+
+    EXPECT_FALSE(res);
 }
