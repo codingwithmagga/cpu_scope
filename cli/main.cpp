88c96838a5d3d5926feef2a5d1c01c9c5fca76bd
codingwithmagga <codingwithmagga@gmail.com>
2026-05-11 12:57:45 +0200

    Remove placeholder code, fix liniting issues

--- cli/main.cpp
+++ cli/main.cpp
@@ -1,26 +1,12 @@
 #include <iostream>
-#include <span>
-#include <string_view>
-#include <vector>
+#include <utility>
 
-#include "cpuscope_lib.hpp"
 #include "perf_event.hpp"
 
 using namespace cpuscope;
 
-int main(int argc, char* argv[])
+int main(int /*argc*/, char** /*argv[]*/)
 {
-    std::vector<std::string_view> args;
-    args.reserve(argc);
-
-    const std::span<char*> arguments(argv, argc);
-    for (char* arg : arguments)
-    {
-        args.emplace_back(arg);
-    }
-
-    std::cout << cpuscope::format_message(args) << '\n';
-
     PerfEvent::LinuxSysCalls sysCalls;
     PerfEvent::Config config;
     config.scope = PerfEvent::Scope::CPU;
--- lib/src/perf_event.cpp
+++ lib/src/perf_event.cpp
@@ -5,6 +5,7 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+#include <cerrno>
 #include <cstdint>
 #include <cstring>
 #include <optional>
