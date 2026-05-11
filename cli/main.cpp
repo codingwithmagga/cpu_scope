4de5a8551e8273ab17376b06ec5ce2acca81818d
codingwithmagga <codingwithmagga@gmail.com>
2026-05-11 13:20:58 +0200

    Add simplified CLI parsing for version flag

--- cli/main.cpp
+++ cli/main.cpp
@@ -1,12 +1,27 @@
+#include <cstring>
 #include <iostream>
+#include <string_view>
 #include <utility>
 
 #include "perf_event.hpp"
 
 using namespace cpuscope;
 
-int main(int /*argc*/, char** /*argv[]*/)
+static constexpr std::string_view VERSION = "0.1.0";
+
+int main(int argc, char** argv)
 {
+    // Simplified parsing of command-line arguments for demonstration purposes
+    // Later add a cli lib like CLI11 or cxxopts for more robust argument parsing
+    for (int i = 1; i < argc; ++i)
+    {
+        if (std::strcmp(argv[i], "--version") == 0)
+        {
+            std::cout << "cpu_scope version " << VERSION << '\n';
+            return 0;
+        }
+    }
+
     PerfEvent::LinuxSysCalls sysCalls;
     PerfEvent::Config config;
     config.scope = PerfEvent::Scope::CPU;
